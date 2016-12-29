fe5e9002609a26b29fc62aa519fabda0b45dbe24
Mohamed Ali Khaskia <mohamedalik88@gmail.com>
2016-12-29 02:59:07 +0200

    First Submit

---
+++ Ceaser Cracking/Ceaser Cracking/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Ceaser_Cracking {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.txtTheCrackedPlan = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCipherCracking = new System.Windows.Forms.TextBox();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btnencyript = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCipher = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPlan = new System.Windows.Forms.Label();
+            this.txtPlan = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdLevel1 = new System.Windows.Forms.RadioButton();
+            this.rdLevel2 = new System.Windows.Forms.RadioButton();
+            this.rdLevel3 = new System.Windows.Forms.RadioButton();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(4, 2);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(444, 439);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.groupBox1);
+            this.tabPage2.Controls.Add(this.txtTheCrackedPlan);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.txtCipherCracking);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(436, 413);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Cracking";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // txtTheCrackedPlan
+            // 
+            this.txtTheCrackedPlan.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.txtTheCrackedPlan.Location = new System.Drawing.Point(6, 332);
+            this.txtTheCrackedPlan.Name = "txtTheCrackedPlan";
+            this.txtTheCrackedPlan.Size = new System.Drawing.Size(424, 27);
+            this.txtTheCrackedPlan.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 166);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(426, 41);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "encyript";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label3.Location = new System.Drawing.Point(26, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Cipher Text";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txtCipherCracking
+            // 
+            this.txtCipherCracking.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.txtCipherCracking.Location = new System.Drawing.Point(144, 55);
+            this.txtCipherCracking.Name = "txtCipherCracking";
+            this.txtCipherCracking.Size = new System.Drawing.Size(286, 27);
+            this.txtCipherCracking.TabIndex = 7;
+            this.txtCipherCracking.Text = "abcd ";
+            this.txtCipherCracking.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btnencyript);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.txtCipher);
+            this.tabPage1.Controls.Add(this.txtKey);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.lblPlan);
+            this.tabPage1.Controls.Add(this.txtPlan);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(436, 413);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "encyription";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btnencyript
+            // 
+            this.btnencyript.Location = new System.Drawing.Point(19, 153);
+            this.btnencyript.Name = "btnencyript";
+            this.btnencyript.Size = new System.Drawing.Size(404, 41);
+            this.btnencyript.TabIndex = 6;
+            this.btnencyript.Text = "encyript";
+            this.btnencyript.UseVisualStyleBackColor = true;
+            this.btnencyript.Click += new System.EventHandler(this.btnencyript_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label2.Location = new System.Drawing.Point(15, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 19);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Cipher";
+            // 
+            // txtCipher
+            // 
+            this.txtCipher.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.txtCipher.Location = new System.Drawing.Point(137, 220);
+            this.txtCipher.Name = "txtCipher";
+            this.txtCipher.Size = new System.Drawing.Size(286, 27);
+            this.txtCipher.TabIndex = 4;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.txtKey.Location = new System.Drawing.Point(137, 106);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(286, 27);
+            this.txtKey.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.label1.Location = new System.Drawing.Point(15, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Key";
+            // 
+            // lblPlan
+            // 
+            this.lblPlan.AutoSize = true;
+            this.lblPlan.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lblPlan.Location = new System.Drawing.Point(15, 42);
+            this.lblPlan.Name = "lblPlan";
+            this.lblPlan.Size = new System.Drawing.Size(75, 19);
+            this.lblPlan.TabIndex = 1;
+            this.lblPlan.Text = "Plan Text";
+            // 
+            // txtPlan
+            // 
+            this.txtPlan.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.txtPlan.Location = new System.Drawing.Point(137, 34);
+            this.txtPlan.Name = "txtPlan";
+            this.txtPlan.Size = new System.Drawing.Size(286, 27);
+            this.txtPlan.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdLevel3);
+            this.groupBox1.Controls.Add(this.rdLevel2);
+            this.groupBox1.Controls.Add(this.rdLevel1);
+            this.groupBox1.Location = new System.Drawing.Point(6, 213);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(153, 91);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Level";
+            // 
+            // rdLevel1
+            // 
+            this.rdLevel1.AutoSize = true;
+            this.rdLevel1.Location = new System.Drawing.Point(30, 20);
+            this.rdLevel1.Name = "rdLevel1";
+            this.rdLevel1.Size = new System.Drawing.Size(31, 17);
+            this.rdLevel1.TabIndex = 0;
+            this.rdLevel1.TabStop = true;
+            this.rdLevel1.Text = "1";
+            this.rdLevel1.UseVisualStyleBackColor = true;
+            // 
+            // rdLevel2
+            // 
+            this.rdLevel2.AutoSize = true;
+            this.rdLevel2.Location = new System.Drawing.Point(30, 43);
+            this.rdLevel2.Name = "rdLevel2";
+            this.rdLevel2.Size = new System.Drawing.Size(31, 17);
+            this.rdLevel2.TabIndex = 1;
+            this.rdLevel2.TabStop = true;
+            this.rdLevel2.Text = "2";
+            this.rdLevel2.UseVisualStyleBackColor = true;
+            // 
+            // rdLevel3
+            // 
+            this.rdLevel3.AutoSize = true;
+            this.rdLevel3.Location = new System.Drawing.Point(30, 66);
+            this.rdLevel3.Name = "rdLevel3";
+            this.rdLevel3.Size = new System.Drawing.Size(31, 17);
+            this.rdLevel3.TabIndex = 2;
+            this.rdLevel3.TabStop = true;
+            this.rdLevel3.Text = "3";
+            this.rdLevel3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(452, 444);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ceaser Cracking";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btnencyript;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCipher;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPlan;
+        private System.Windows.Forms.TextBox txtPlan;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCipherCracking;
+        private System.Windows.Forms.TextBox txtTheCrackedPlan;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdLevel3;
+        private System.Windows.Forms.RadioButton rdLevel2;
+        private System.Windows.Forms.RadioButton rdLevel1;
+    }
+}
+
---
+++ Ceaser Cracking/Ceaser Cracking/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using NetSpell.SpellChecker;
+using NetSpell.SpellChecker.Dictionary;
+
+namespace Ceaser_Cracking {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+        public int TheKey;
+        string space = "abcdefghijklmnopqrstuvwxyz ";
+
+        public void optimiseKey() {
+            TheKey = Convert.ToInt32(txtKey.Text);
+            for (int m = 2; m < 100; m++) {
+                if (TheKey > 26 && TheKey < 26 * m) {
+                    TheKey = TheKey - (26 * (m - 1));
+                    MessageBox.Show(TheKey + "");
+                    break;
+                }
+            }
+        }
+
+
+
+        private void btnencyript_Click(object sender, EventArgs e) {
+            optimiseKey();
+
+
+
+            //  textcase += Convert.ToChar(cipher[i] - x);
+            //  textcase += space[x];
+
+
+
+
+
+            string plan = txtPlan.Text;
+            int added = 0;
+            string cipehrout = "";
+            /*    string cipher = "";
+              for (int i = 0; i < txtPlan.Text.Length; i++)
+              {
+                  cipher += Convert.ToChar( TheKey + plan[i]);
+
+              }
+
+              txtCipher.Text = cipher;
+              //  theAsciNumer = TheCode + RandomChars.IndexOf(cylinders[i].Text);
+
+              */
+            for (int i = 0; i < plan.Length; i++) {
+
+                int theAsciNumer = (space.IndexOf(plan[i]) + TheKey);
+                // cipehrout = cipehrout + space[i];
+                //  MessageBox.Show("the AsciNumber is" + theAsciNumer);
+                if (theAsciNumer >= 27) {
+
+                    try {
+                        added = theAsciNumer - 27 + 0;
+                        cipehrout += space[added];
+                    }
+                    catch (Exception ee) {
+                        MessageBox.Show(ee.Message);
+                    }
+                }
+                else {
+                    cipehrout += space[theAsciNumer]; // true
+                }
+            }
+            txtCipher.Text = cipehrout;
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e) {
+
+        }
+
+        bool check(string word) {
+            WordDictionary oDict = new WordDictionary();
+            oDict.DictionaryFile = "en-US.dic";
+            oDict.Initialize();
+            string wordToCheck = word;
+            Spelling oSpell = new Spelling();
+
+            oSpell.Dictionary = oDict;
+            if (!oSpell.TestWord(wordToCheck)) {
+                // MessageBox.Show("the word doen't exist");
+                return false;
+            }
+            else {
+                //  MessageBox.Show("the word is exest");
+                return true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtTheCrackedPlan.Text = "";
+            string space = "abcdefghijklmnopqrstuvwxyz ";
+            string cipher = txtCipherCracking.Text;
+            string textcase = "";
+            List<string> TestCasesList = new List<string>();
+            int MinValue = 0;
+            MessageBox.Show(Convert.ToString(cipher.IndexOf(" ")));
+            for (int x = 0; x < 26; x++) {
+                for (int i = 0; i < cipher.Length; i++) {
+                    int theAsciNumer = (space.IndexOf(cipher[i]) - x);
+                    //  MessageBox.Show("the AsciNumber is" + theAsciNumer);
+                    if (theAsciNumer < 0) {
+                        MinValue = (26 - (-1 - theAsciNumer));
+                        try {
+                            textcase += space[MinValue];
+                        }
+                        catch (Exception ee) {
+                            MessageBox.Show(ee.Message);
+                        }
+                    }
+                    else {
+                        textcase += space[theAsciNumer]; // true
+                    }
+
+                    //  textcase += Convert.ToChar(cipher[i] - x);
+                    //  textcase += space[x];
+
+
+
+                }
+                TestCasesList.Add(textcase);
+                textcase = "";
+            }
+
+            bool IsValidWord1;
+            bool IsValidWord2;
+            bool IsValidWord3;
+            string Word1 = "";
+            string Word2 = "";
+            string Word3 = "";
+            string Allcases = "";
+            string PlanText = "";
+            foreach (var VARIABLE in TestCasesList) {
+                Allcases += VARIABLE + "\n";
+            }
+            MessageBox.Show(Allcases);
+
+            foreach (var VARIABLE in TestCasesList) {
+                try {
+
+                    Word1 = VARIABLE.Split(' ')[0];
+                    Word2 = VARIABLE.Split(' ')[1];
+                    Word3 = VARIABLE.Split(' ')[2];
+                    
+
+                    if (rdLevel3.Checked) {
+                        IsValidWord1 = check(Word1);
+                        IsValidWord2 = check(Word2);
+                        IsValidWord3 = check(Word3);
+                        if (IsValidWord1 && IsValidWord2 && IsValidWord3) {
+                            PlanText += VARIABLE + " Key=" + TestCasesList.IndexOf(VARIABLE) + "\n";
+                        }
+                    }
+                    else if (rdLevel2.Checked)
+                    {
+                        IsValidWord1 = check(Word1);
+                        IsValidWord2 = check(Word2);
+                        
+                        if (IsValidWord1 && IsValidWord2) {
+                            PlanText += VARIABLE + " Key=" + TestCasesList.IndexOf(VARIABLE) + "\n";
+                        }
+                    }
+                    else if (rdLevel1.Checked) {
+                        IsValidWord1 = check(Word1);
+                        
+                        if (IsValidWord1) {
+                            PlanText += VARIABLE + " Key=" + TestCasesList.IndexOf(VARIABLE) + "\n";
+                        }
+                    }
+
+
+                }
+                catch {
+
+                }
+            }
+            MessageBox.Show(PlanText);
+            txtTheCrackedPlan.Text = PlanText;
+        }
+    }
+}
